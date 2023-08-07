1ff813b81fcd7aea493851b867a8a63392b75928
alleth <allethrey@gmail.com>
2023-08-07 19:38:36 +0800

    rename javascript link

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,7 @@
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 
-<script type="text/javascript" src="assest/js/index.js"></script>
-<script type="text/javascript" src="assest/js/page-content.js"></script>
-<script type="text/javascript" src="assest/js/script.js"></script>
+<script type="text/javascript" src="assets/js/index.js"></script>
+<script type="text/javascript" src="assets/js/page-content.js"></script>
+<script type="text/javascript" src="assets/js/script.js"></script>
 </html>
