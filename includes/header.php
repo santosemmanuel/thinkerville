7b93529594764d0996096ec01c3a6eae5cc6812d
Emmanuel <santosemmanuel17@gmail.com>
2023-08-07 17:20:52 +0800

    "nice one pre"

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  
+
     <link rel="stylesheet" href="assest/css/styles.css" />
+    <?php define("Base_Url", "localhost:");  ?>
 </head>
