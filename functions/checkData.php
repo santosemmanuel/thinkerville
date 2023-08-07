af2335659a2fb50878043355591cc0fd109457fe
alleth <allethrey@gmail.com>
2023-08-07 21:16:16 +0800

    php edit

--- functions/checkData.php
+++ functions/checkData.php
@@ -3,9 +3,8 @@ include "DAO/checkDataDAO.php";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $answerData = $_POST["userInputData"];
-    $totalNum = $_POST["totalNum"];
 } else {
         echo "invalid request";
 }
 $action = new checkDataDAO();
-$action->checkDataFunctions($answerData, $totalNum);
+$action->checkDataFunctions($answerData);
