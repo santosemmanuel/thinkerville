20f85aab0080bb82bc57181cc8504c0e03899769
Emmanuel <santosemmanuel17@gmail.com>
2023-09-23 08:46:57 +0800

    "endoresments final page ongoing production"

--- endoresments.php
+++ endoresments.php
@@ -2,21 +2,21 @@
 
 <div style='background-color: #253C78;' class="h-100 p-5">
 
-    <div class="container bg-white h-100 mx-auto p-4 rounded-3" style="width: 80%">
+    <div class="shadow container bg-white h-100 mx-auto p-4 rounded-3" style="width: 80%">
         
-        <h2 class="text-center">Pass the actual civil Service Exam!</h2>
+        <h1 class="text-center fw-bolder py-3">Pass the actual Civil Service Exam!</h1>
 
-        <p class="text-center">75% OFF</p>
+        <p class="text-center fw-bold mb-5" style="font-size: 90px;">75% OFF</p>
 
         <div class="row">
             <div class="col">
-                <h6 class="text-center">Before</h6>
-                <p class="text-center"><del>2,000</del></p>
+                <h6 class="text-center fw-bold" style="font-size: 30px;">Before</h6>
+                <p class="text-center fw-bolder" style="font-size: 60px;">&#8369; <del>2,000.00</del></p>
             </div>
             
             <div class="col">
-                <h6 class="text-center">Now</h6>
-                <p class="text-center">500</p>
+                <h6 class="text-center fw-bold" style="font-size: 30px;">Now</h6>
+                <p class="text-center fw-bolder" style="font-size: 60px;">&#8369; 500.00</p>
             </div>
           
         </div>
@@ -46,4 +46,25 @@
 
 </div>
 
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop='static'>
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header bg-danger">
+        <h5 class="modal-title text-white" id="exampleModalLabel"><i class="fa-solid fa-lightbulb"></i>&nbsp; We have something special just for you!</h5>
+      </div>
+      <div class="modal-body p-4 text-center">
+        <h1 class="py-5">Ace the actual <b>CSE</b> for <b>75%</b> OFF!</h1>
+        <button type="button" class="btn btn-primary btn-lg mb-5" data-bs-dismiss="modal" aria-label="Close">Check details here</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <?php include("includes/footer.php"); ?>
+<script type="text/javascript">
+   var myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+   myModal.show();
+</script>
+
+
