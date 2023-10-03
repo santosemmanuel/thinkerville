5fa0c3a103da46d03792f16ff4e9f225c9ca496b
alleth <allethrey@gmail.com>
2023-10-03 08:44:59 +0800

    update

--- functions/DAO/display-vidDAO.php
+++ functions/DAO/display-vidDAO.php
@@ -5,12 +5,12 @@ class displayVidDAO extends BaseDAO {
 
     function displayVideos(){
         $this->openConn();
-        $stmt = $this->dbh->prepare("SELECT * FROM tbl_vid ORDER BY RAND() LIMIT 1");
+        $stmt = $this->dbh->prepare("SELECT * FROM tbl_vid ORDER BY RAND()");
         $stmt->execute();
         $this->closeConn();
 
         while ($row = $stmt->fetch()) {
-            echo "<video src='". $row[1] ."' controls width='370' height='210'>
+            echo "<video id='vid-player'src='". $row[1] ."' controls width='370' height='210'>
                     Your browser does not support the video tag.
                 </video>";
         }
