ea84b110269d07ca40b513706037f09686cf50dc
alleth <allethrey@gmail.com>
2023-10-19 14:48:13 +0800

    small updates - countdown

--- assets/js/countdown-promo.js
+++ assets/js/countdown-promo.js
@@ -16,7 +16,6 @@ function countdownPromo(){
             var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
             var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-            // Add leading zeros
             days = String(days).padStart(2, '0');
             hours = String(hours).padStart(2, '0');
             minutes = String(minutes).padStart(2, '0');
