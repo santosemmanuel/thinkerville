7ecb8883ed7bee0556b351354ad01c924ea69a5c
Emmanuel <santosemmanuel17@gmail.com>
2023-12-14 09:27:23 +0800

    "Endoresement Video discussion revision"

--- endoresments.php
+++ endoresments.php
@@ -112,41 +112,29 @@
       </div>
     </div>
   </div>
-  
+
   <div class="row bg-warning p-5">
-    <h1 class="text-center fw-bolder mb-4">Video Discussions</h1>
-    <ul class="nav nav-pills mb-3 justify-content-center mb-4" id="pills-tab" role="tablist">
-      <li class="nav-item" role="presentation">
-        <button class="nav-link active fw-bolder" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true"><i class="fa-solid fa-language"></i>&nbsp; Vocabulary</button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button class="nav-link fw-bolder" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false"><i class="fa-solid fa-square-root-variable"></i>&nbsp; PEMDAS</button>
-      </li>
-      <li class="nav-item" role="presentation">
-        <button class="nav-link fw-bolder" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false"><i class="fa-solid fa-scale-balanced"></i>&nbsp; Article 1: The National Territory</button>
-      </li>
-    </ul>
-    <div class="tab-content" id="pills-tabContent">
-      <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-        <center>
-          <div class="ratio ratio-21x9 video_discuss" >
-            <iframe src="https://drive.google.com/file/d/1B1qjG5GkgEneDBTDkqE8DGhKkU2jX1gb/preview" allowfullscreen></iframe>
-          </div>
-        </center>
-      </div>
-      <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
-        <center>
+    <h1 class="text-center fw-bolder mb-5">Video Discussions</h1>
+    <div class="container text-center">
+      <div class="row align-items-end">
+        <div class="col-md-4">
+          <h5><i class="fa-solid fa-scale-balanced"></i>&nbsp; Evolution of the Philippine Constitution</h5>
           <div class="ratio ratio-21x9 video_discuss">
-            <iframe src="https://drive.google.com/file/d/1Nn-VE7PQ4c-Xiy85IlsG-dw6m8T1w9P1/preview" allowfullscreen></iframe>
+            <iframe src="https://drive.google.com/file/d/1jFnNpxp_Mbymy7-UTJcM8H1OoSzVY9Ff/preview" allowfullscreen></iframe>
           </div>
-        </center>
-      </div>
-      <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
-      <center>
-        <div class="ratio ratio-21x9 video_discuss">
-          <iframe src="assets/vids/Article 1.mp4" title="YouTube video" allowfullscreen ></iframe>
         </div>
-          </center>
+        <div class="col-md-4">
+          <h5><i class="fa-solid fa-square-root-variable"></i>&nbsp; PEMDAS</h5>
+          <div class="ratio ratio-21x9 video_discuss">
+            <iframe src="https://drive.google.com/file/d/1jEiMTc-A28cvO2QQtyWpaQI76ZiyQrAq/preview" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <h5><i class="fa-solid fa-language"></i>&nbsp; Vocabulary</h5>
+          <div class="ratio ratio-21x9 video_discuss">
+            <iframe src="https://drive.google.com/file/d/1tIPogfD0DiD30VIPVOrEziJqZFABRQ32/preview" allowfullscreen></iframe>
+          </div>
+        </div>
       </div>
     </div>
   </div>
